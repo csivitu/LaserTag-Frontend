4fdfa9960ba46a850fdcf154203cde9d64449e17
adityak840 <khandelwal.aditya840@gmail.com>
2022-09-17 01:07:02 +0530

    Added Pagination

--- src/SlotBooking.js
+++ src/SlotBooking.js
@@ -1,22 +1,37 @@
-import {useEffect, useState} from 'react'
-import './css/App.css'
+import {useEffect, useState} from 'react';
+import {Box, Pagination} from '@mui/material';
+import './css/App.css';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const SlotBooking = () => {
     const [show, setShow] = useState(-1);
+    const [isPending, setIsPending] = useState(true);
     const timeList = ['10:15 AM','10:30 AM','10:45 AM','11:00 AM',
     '01:45 PM','02:00 PM','02:15 PM','02:30 PM','02:45 PM','03:00 PM',
     '03:15 PM','03:30 PM','05:00 PM','05:15 PM','05:30 PM','05:45 PM'];
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("http://localhost:8000")
+            .then(res=>{
+                setIsPending(false);
+            });
+        },1000);
+        
+    })
     return (
         <div className='stuff'>
             <div className='day'>
                 <p>Available Slots</p>
-                <h10>Today</h10>
+                <Box>
+                    <Pagination 
+                    className='Pagination' 
+                    count = {3}
+                    />                    
+                </Box>
             </div>
             <div className='slots'>
-                {timeList.map((time, index) => (
-                    <button onClick={() => setShow(index)} id = {`time${index}`} key = {index} className='Button'>{time}</button>
-                ))}
+                {isPending ? <div className="Loading">Loading ....</div> : 
+                timeList.map((time, index) => (<button onClick={() => setShow(index)} id = {`time${index}`} key = {index} className='Button'>{time}</button>))}
             </div>
             {
                 (show !== -1) && <button className='Book'><p>Confirm Booking</p><ArrowForwardIcon /></button >
--- src/TeamDetails.js
+++ src/TeamDetails.js
@@ -1,11 +1,20 @@
+import {useEffect, useState} from 'react'
 import './css/App.css'
 
 const TeamDetails = () => {
+    const [teamName, setTeamName] = useState('');
+    const [teamSize, setTeamSize] = useState('');
     return(
         <div className='TeamDetails'>
             <div className='Size'>
                 <label for="number_players">Number of Players</label>
-                <select name="number_players" id = "number_players">
+                <select 
+                name="number_players" 
+                id = "number_players"
+                required
+                value = {teamSize}
+                onChange = {(e) => setTeamSize(e.target.value)}
+                >
                 <option value ="1">1</option>
                 <option value ="2">2</option>
                 <option value ="3">3</option>
@@ -13,12 +22,15 @@ const TeamDetails = () => {
             </div>
             <div className='Name'>
                 <label for="teamName">Name of the Team</label>
-                <input type="teamName"></input>
+                <input 
+                type="teamName"
+                required
+                value = {teamName}
+                onChange = {(e) => setTeamName(e.target.value)}
+                />
             </div>
         </div>
-        
     )
-    
 }
 
 export default TeamDetails
