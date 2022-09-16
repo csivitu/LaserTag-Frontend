d0cf3f89df78f34e35e811fef62c8cab68c717ea
adityak840 <khandelwal.aditya840@gmail.com>
2022-09-16 22:27:24 +0530

    Added Id to Buttons

--- src/SlotBooking.js
+++ src/SlotBooking.js
@@ -3,7 +3,7 @@ import './css/App.css'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const SlotBooking = () => {
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(-1);
     const timeList = ['10:15 AM','10:30 AM','10:45 AM','11:00 AM',
     '01:45 PM','02:00 PM','02:15 PM','02:30 PM','02:45 PM','03:00 PM',
     '03:15 PM','03:30 PM','05:00 PM','05:15 PM','05:30 PM','05:45 PM'];
@@ -14,12 +14,12 @@ const SlotBooking = () => {
                 <h10>Today</h10>
             </div>
             <div className='slots'>
-                {timeList.map((time) => (
-                    <button onFocus={() => setShow(!show)} className='Button'>{time}</button>
+                {timeList.map((time, index) => (
+                    <button onClick={() => setShow(index)} id = {`time${index}`} key = {index} className='Button'>{time}</button>
                 ))}
             </div>
             {
-                show && <button className='Book'><p>Confirm Booking</p><ArrowForwardIcon /></button >
+            (show !== -1) ? null : <button className='Book'><p>Confirm Booking</p><ArrowForwardIcon /></button >
             }
         </div>     
     )
