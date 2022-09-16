61b569e0e2cbfe8b3dc3c640e2ac32ecb4d04482
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2022-09-16 21:54:32 +0530

    second commit

---
+++ src/ImageSlider.js
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+
+const slideStyles = {
+  top: "40px",
+  width: "100%",
+  height: "100%",
+  borderRadius: "10px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const rightArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  right: "30px ",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const leftArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  left: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const sliderStyles = {
+  position: "relative",
+  height: "100%",
+};
+
+const dotsContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+};
+
+const dotStyle = {
+  margin: "0 3px",
+  cursor: "pointer",
+  fontSize: "20px",
+};
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const slideStylesWidthBackground = {
+    ...slideStyles,
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
+  return (
+    <div id = "ImageSlider" style={sliderStyles}>
+      <div>
+        <div onClick={goToPrevious} style={leftArrowStyles}>
+          <KeyboardDoubleArrowLeftIcon />
+        </div>
+        <div onClick={goToNext} style={rightArrowStyles}>
+          <KeyboardDoubleArrowRightIcon />
+        </div>
+      </div>
+      <div style={slideStylesWidthBackground}></div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <div
+            style={dotStyle}
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ●
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
---
+++ src/Slideshow.js
@@ -0,0 +1,45 @@
+// import ImageSlider from "./ImageSlider";
+
+// const Slideshow = () => {
+//   const slides=[
+//     {url: 'http://localhost:3000/image-1.jpg', title:'Beach'},
+//     {url: 'http://localhost:3000/image-2.jpg', title:'Boat'},
+//     {url: 'http://localhost:3000/image-3.jpg', title:'Forest'},
+//     {url: 'http://localhost:3000/image-4.jpg', title:'City'},
+//     {url: 'http://localhost:3000/image-5.jpg', title:'Italy'}
+//   ];
+//   const containerStyles ={
+//     width: '500px',
+//     height:'280px',
+//     margin:'0 auto',
+
+//   }
+// }
+
+import ImageSlider from "./ImageSlider";
+import './css/App.css';
+
+const Slideshow = () => {
+  const slides = [
+    { url: "https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/6eetog61ec16aftpms3reuu2jtz9_shutterstock_1458739901.jpg", title: "slide1" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWMyZDFQUyvo6yl-jY3yvRUXA9ENt1RX4d2Q&usqp=CAU", title: "slide2" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2WtlBuyfXXp5dmbSGrYTS56LT0uLWQXYC7w&usqp=CAU", title: "slide3" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ht3IU9wX3ep3r4R4ecwIOQCi75enlUO4DQ&usqp=CAU", title: "slide4" },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6Gp1zu73rfQbu-Q1kM75Z8CxFZeg46DlEVA&usqp=CAU", title: "slide5" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
+  return (
+    <div>
+
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;
