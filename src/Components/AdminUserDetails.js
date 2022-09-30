0d53348d3566820c08a3b9a4a836f978ed023e2f
nimishjn <nimishjain100701@gmail.com>
2022-09-30 11:50:33 +0530

    feat: Admin Panel

---
+++ src/Components/AdminUserDetails.js
@@ -0,0 +1,81 @@
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
+import { FaChevronDown } from 'react-icons/fa';
+import { getTime } from '../util/processSlotData';
+import { BsFillCheckCircleFill, BsFillXCircleFill } from 'react-icons/bs';
+
+const AdminUserDetails = ({ user, key }) => {
+	return (
+		<Accordion key={key}>
+			<AccordionSummary
+				expandIcon={<FaChevronDown />}
+				aria-controls='panel1a-content'
+				id='panel1a-header'
+			>
+				<div className='grid grid-cols-3 items-center justify-items-center w-full'>
+					<p className='break-all text-xs sm:text-sm p-2 m-0'>
+						{user.name}
+					</p>
+					<p className='break-all text-xs sm:text-sm p-2 m-0'>
+						{user.username}
+					</p>
+					<p className='break-all text-xs sm:text-sm p-2 m-0'>
+						{user.email}
+					</p>
+				</div>
+			</AccordionSummary>
+			<AccordionDetails>
+				<div className='p-2 bg-gray-modal rounded-md w-90 max-w-full m-2 border-1 border-solid border-white flex flex-col justify-center items-center'>
+					<p className='my-1 break-all'>{user.name}</p>
+					<p className='my-1 break-all'>{user.email}</p>
+					<p className='my-1 break-all'>{user.username}</p>
+					<p className='text-sm m-0'>
+						<b>Payment: </b>
+                        <Status value={user.isPaid} />
+					</p>
+                    <p className='text-sm m-0'>
+						<b>Slot Changed: </b>
+                        <Status value={user.isChangedSlot} />
+					</p>
+                    <p className='text-sm m-0'>
+						<b>Scanned: </b>
+                        <Status value={user.isScanned} />
+					</p>
+					<h3 className='my-2'>Slot Details:</h3>
+					{user.slotBooked ? (
+						<>
+							<p className='text-sm m-0'>
+								{getTime(user.slotBooked.startTime)}
+								{' - '}
+								{getTime(user.slotBooked.endTime)}
+							</p>
+							<p className='text-sm m-0'>
+								{new Date(
+									user.slotBooked.startTime
+								).toDateString()}
+							</p>
+							<p className='text-sm m-0'>SJT 308</p>
+						</>
+					) : (
+						<p className='text-sm m-0 text-stone-500'>
+							<i>Not booked yet!</i>
+						</p>
+					)}
+				</div>
+			</AccordionDetails>
+		</Accordion>
+	);
+};
+
+export const Status = ({ value }) => {
+	return (
+		<>
+			{value ? (
+				<BsFillCheckCircleFill className='text-green-600 -mb-0.5' />
+			) : (
+				<BsFillXCircleFill className='text-red-500 -mb-0.5' />
+			)}
+		</>
+	);
+};
+
+export default AdminUserDetails;
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,83 +1,207 @@
-import React, {useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
-import { getAllData } from "../Components/axios";
+import React, { useEffect, useState } from 'react';
+import { getAllData } from '../Components/axios';
 import Lottie from 'lottie-react';
 import lasertagLogo from '../lottie/loading.json';
+import {
+	Button,
+	FormControl,
+	FormControlLabel,
+	Radio,
+	RadioGroup,
+} from '@mui/material';
+import AdminUserDetails from '../Components/AdminUserDetails';
 
 const Admin = () => {
-    const [view,setView] = useState(false);
-    const [error, setError] = useState(false);
-    const [info, setInfo] = useState([]);
-    const style1 = {
-        display: "flex",
-        gap: "20%",
-        width: "900px",
-        marginLeft:"15%",
-    }
-    useEffect(()=> {
-        const getRequest = async() => {
-            const res = await getAllData();
-            if (res.success){
-                setView(true);
-                setInfo(res.data);
-                console.log(res);
-            }   
-            else{
-                setError(true);
-                setView(false);
-            }
-        };
-        getRequest();
-    });
-    return(
-        <div className="Details">
-            {
-                view? (
-                <div>
-                    <div
-                    style={style1}
-                    >
-                        <p>Name</p>
-                        <p>Username</p>
-                        <p>Email</p>
-                    </div>
-                    <div>
-                        {
-                            info.map((data) => (
-                                <>
-                                    <p>{data.name}</p>
-                                    <p>{data.username}</p>
-                                    <p>{data.email}</p>
+	const [errorMesaage, setErrorMessage] = useState('');
+	const [errorCode, setErrorCode] = useState(0);
+	const [info, setInfo] = useState([]);
+	const [filteredInfo, setFilteredInfo] = useState([]);
+	const [search, setSearch] = useState('');
+	const [radioValue, setRadioValue] = useState('all');
 
-                                </>
-                            ))
-                        }
-                    </div>
-                </div>):(
-                <>
-                    <Lottie
+	const isRadioTrue = (user) => {
+		if (radioValue === 'all') return true;
+		else if (radioValue === 'paid') return user.isPaid;
+		else if (radioValue === 'unpaid') return !user.isPaid;
+		else if (radioValue === 'booked') return user.slotBooked !== null;
+		else if (radioValue === 'notBooked') return user.slotBooked === null;
+		else return true;
+	};
+
+	const isSearchTrue = (user) => {
+		if (search === '') setFilteredInfo(info);
+
+		return (
+			user.username.toLowerCase().includes(search.toLowerCase()) ||
+			user.name.toLowerCase().includes(search.toLowerCase()) ||
+			user.email.toLowerCase().includes(search.toLowerCase())
+		);
+	};
+
+	const filterUsers = (users) => {
+		setFilteredInfo(
+			users.filter((user) => isRadioTrue(user) && isSearchTrue(user))
+		);
+	};
+
+	const handleSearch = () => {
+		filterUsers(info);
+	};
+
+	useEffect(() => {
+		const getRequest = async () => {
+			setInfo([]);
+			setFilteredInfo([]);
+			const res = await getAllData();
+			if (res.success) {
+				setInfo(res.data);
+				setFilteredInfo(res.data);
+			} else {
+				setErrorCode(res.code);
+				setErrorMessage(res.message);
+			}
+		};
+		getRequest();
+	}, []);
+	return (
+		<div className='w-screen h-screen flex flex-col items-center px-2 pt-5 max-w-5xl m-auto'>
+			{info.length > 0 && (
+				<>
+					<div className='w-full fixed z-50 bg-black flex flex-col p-2 -mt-5 max-w-5xl m-auto'>
+						<div className='flex gap-2'>
+							<input
+								id='search-input'
+								placeholder='Serach for anything'
+								className='border-1 border-solid border-white w-full bg-stone-900 text-white outline-none rounded-md px-2'
+								value={search}
+								onChange={(e) => setSearch(e.target.value)}
+								onKeyDown={(e) =>
+									e.key === 'Enter' && handleSearch()
+								}
+							/>
+							<Button variant='contained' onClick={handleSearch}>
+								Search
+							</Button>
+						</div>
+						<FilterRadioGroup
+							radioValue={radioValue}
+							setRadioValue={setRadioValue}
+						/>
+						<div className='pl-4 pr-8 text-lg text-center font-bold grid grid-cols-3 justify-between align-center mt-3 border-t-2 border-solid border-stone-500'>
+							<p className='my-1'>Name</p>
+							<p className='my-1'>Username</p>
+							<p className='my-1'>Email</p>
+						</div>
+					</div>
+
+					<div className='text-center pt-40 w-full'>
+						{filteredInfo.length ? (
+							<>
+								<p className='text-xs m-0 text-center text-stone-500'>
+									{filteredInfo.length} users found.
+								</p>
+								{filteredInfo
+									.sort((a, b) =>
+										a.name
+											.toLowerCase()
+											.localeCompare(b.name.toLowerCase())
+									)
+									.map((data, index) => (
+										<AdminUserDetails
+											user={data}
+											key={index}
+										/>
+									))}
+							</>
+						) : (
+							<p className='text-center'>No users found!</p>
+						)}
+					</div>
+				</>
+			)}
+			{errorCode ? (
+				<>
+					<h1 className='my-2'>{errorCode}</h1>
+					<p className='my-2 text-center max-w-xl'>{errorMesaage}</p>
+				</>
+			) : (
+				<>
+					<Lottie
 						animationData={lasertagLogo}
 						className='w-full sm:w-3/5 md:w-1/4'
 					/>
 					<p className='text-lg -mt-5'>Loading... Please wait</p>
-                </>
-                )
-            }
-            {
-                error?(
-                    <div>
-                        <Lottie
-						animationData={lasertagLogo}
-						className='w-full sm:w-3/5 md:w-1/4'
-					    />
-					    <p className='text-lg -mt-5'>Error in fetching data.../p>
-                    </div>
-                ) : (
-                    null
-                )
-            }
-        </div>
-    )
-}
+				</>
+			)}
+		</div>
+	);
+};
+
+const FilterRadioGroup = ({ radioValue, setRadioValue }) => {
+	return (
+		<FormControl>
+			<RadioGroup
+				row
+				aria-labelledby='demo-row-radio-buttons-group-label'
+				name='row-radio-buttons-group'
+				value={radioValue}
+				onChange={(e) => setRadioValue(e.target.value)}
+			>
+				<div className='grid grid-cols-5 w-full'>
+					<FormControlLabel
+						value='all'
+						control={<Radio />}
+						labelPlacement='bottom'
+						className='text-center m-0'
+						classes={{
+							label: '!text-xs sm:!text-sm',
+						}}
+						label='All'
+					/>
+					<FormControlLabel
+						value='paid'
+						control={<Radio />}
+						labelPlacement='bottom'
+						className='text-center m-0'
+						classes={{
+							label: '!text-xs sm:!text-sm',
+						}}
+						label='Paid'
+					/>
+					<FormControlLabel
+						value='unpaid'
+						control={<Radio />}
+						labelPlacement='bottom'
+						className='text-center m-0'
+						classes={{
+							label: '!text-xs sm:!text-sm',
+						}}
+						label='Not Paid'
+					/>
+					<FormControlLabel
+						value='booked'
+						control={<Radio />}
+						labelPlacement='bottom'
+						className='text-center m-0'
+						classes={{
+							label: '!text-xs sm:!text-sm',
+						}}
+						label='Booked'
+					/>
+					<FormControlLabel
+						value='notBooked'
+						control={<Radio />}
+						labelPlacement='bottom'
+						className='text-center m-0'
+						classes={{
+							label: '!text-xs sm:!text-sm',
+						}}
+						label='Not Booked'
+					/>
+				</div>
+			</RadioGroup>
+		</FormControl>
+	);
+};
 
 export default Admin;
